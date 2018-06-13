5a5a4212856e430bfd2961a8a4d17240209af8f4
Jacob Kjaergaard <upperiq@hotmail.com>
2018-06-13 23:12:35 +0200

    Starting point

---
+++ AIDomination/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+	
+	[SerializeField] [Range(1f,20f)] private float _gridSize = 10f;
+
+	private TextMesh _textMesh;
+
+	void Start()
+	{
+		_textMesh = GetComponentInChildren<TextMesh>();
+	}
+
+	void Update()
+	{
+		Vector3 snapPos;
+		snapPos.x = Mathf.RoundToInt(transform.position.x / _gridSize) * _gridSize;
+		snapPos.y = 0f;
+		snapPos.z = Mathf.RoundToInt(transform.position.z / _gridSize) * _gridSize;
+
+		transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+		string _labelText = snapPos.x / _gridSize + "," + snapPos.z / _gridSize;
+		_textMesh.text = _labelText;
+		gameObject.name = "Cube: " + _labelText;
+	}
+	
+}
